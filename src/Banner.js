c619289a33a531c9776b63f0b8f97b7bc0097c0a
Tiyasbiswas <tiyasbiswas17@gmail.com>
2022-07-22 16:56:02 +0200

    Banner file added

--- src/Banner.js
+++ src/Banner.js
@@ -1,21 +1,24 @@
 
+
+import Ratio from 'react-bootstrap/Ratio';
+
 export default function Banner(){
 return(
    <div class="container">
     <div class="row">
     <div class="col-sm-6">
-      <img src="https://images.unsplash.com/photo-1536625925840-616261910c62?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJlJTIwd2Fsa2luZ3xlbnwwfHwwfHw%3D&w=250&q=100" alt=""/>
-    </div>
-    <div class="col-sm-6">
-    <p>  
+     <img src="https://images.pexels.com/photos/591216/pexels-photo-591216.jpeg?auto=compress&cs=tinysrgb&w=560&h=350&dpr=1" alt="banner logo"/>
+     
+     </div>  
+       <div class="col-sm-6">
+        <p>  
           I am happy to walk down the road of life with you, my friend. We make good travel buddies. For us,
           it’s not about reaching a destination. Just spending time with you is all I need to feel happy in life.
-
           It helps in life to have a friend like you to walk home with. The love in our friendship guides the heart to heaven as God is in the middle.
           I hope he blesses us to stay friends forever. Our guardian angels must also be friends.
           If I could have one person to walk with me through life, I’d choose you. You are well prepared, practical, compassionate, 
           and easy to talk to. You have wisdom that gently inspires me. I know I don’t tell you enough, but thank you for being in my life.
-   </p>   
+       </p>   
  </div>
     
   </div>
