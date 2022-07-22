7de8772366b6cf7cf93f530ba1500d877ca0aee5
Neha <nehashaikh470@gmail.com>
2022-07-22 16:52:06 +0200

    header complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,29 +5,19 @@ import { Container, Button } from 'react-bootstrap'
 export default function Header() {
     return (
         <section>
-        <Navbar  expand="lg">
+        <Navbar className="main-header" expand="lg">
             <Container fluid>
-            <Navbar.Brand href=""><img src="./images/logo1.png"/>
-           </Navbar.Brand>
+            <Navbar.Brand href=""><img src="./images/bigfont-logo.png"/></Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
-            <Nav
-        className="me-auto my-2 my-lg-0"
-        style={{ maxHeight: '100px' }}
-        navbarScroll
-      >
-        <Nav.Link href="#action1">Home</Nav.Link>
-        <Nav.Link href="#action2">About</Nav.Link>
-        <Nav.Link href="#" disabled>
-          Blog
-        </Nav.Link>
-        <Nav.Link href="#" disabled>
-          Contact
-        </Nav.Link>
-      </Nav>
-        <Button variant="outline-success">Login</Button>
-
-    </Navbar.Collapse>
+            <Nav className="me-auto my-2 my-lg-0 justify-content-end" style={{ maxHeight: '100px', gap: '40px'}} navbarScroll>
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">About</Nav.Link>
+              <Nav.Link href="#">Blog</Nav.Link>
+              <Nav.Link href="#">Contact</Nav.Link>
+            </Nav>
+            <Button className="mr-5" variant="outline-success" >Login</Button>
+            </Navbar.Collapse>
   </Container>
 </Navbar>
 </section>
