5eefa5afc3739d6eaa024dfa2a1087b5b21840f8
Tiyasbiswas <tiyasbiswas17@gmail.com>
2022-07-22 17:00:17 +0200

    Banner file place modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Acitivity from './components/Activity';
-import Banner from './Banner';
+import Banner from './components/Banner';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
