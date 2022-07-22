b0aba66a7caed8e0f4a7ad473da6c906e4dbe61f
Tiyasbiswas <tiyasbiswas17@gmail.com>
2022-07-22 15:31:38 +0200

    changes added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Header from './components/Header';
 import Acitivity from './components/Activity';
+import Banner from './Banner';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-        <Header />
+      <Header />
+      <Banner/>
       <Acitivity/>
       <Footer/>
     </div>
--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,42 +1,38 @@
-import Carousel from 'react-bootstrap/Carousel'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 export default function Acitivity() {
     return (
-<Carousel fade>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
-      alt="Second slide"
-    />
+      <>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Activity 1</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
-      alt="Third slide"
-    />
+    <Card style={{ width: '20rem' }}>
+      <Card.Body>
+        <Card.Title>Activity 2</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
 
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+    <Card style={{ width: '20rem' }}>
+      <Card.Body>
+        <Card.Title>Activity 3</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    </>
   )
 }
