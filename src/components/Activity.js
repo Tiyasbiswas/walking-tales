bd1ed72c9dfcf9be3edb79d0480b4eefd90d046b
Neha <nehashaikh470@gmail.com>
2022-07-25 10:32:37 +0200

    resolve conflicts

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,17 +1,66 @@
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 export default function Acitivity() {
     return (
-      
-    <>
-    <div>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-            
-        </Card>
+      <div className="container">
+      <div className="row">
+         <div className="col-sm-6">
+          <p  >  
+          “Yoga is not a work-out it is a work-in, and this is the point of spiritual practice to make us teachable to open up our hearts and focus our awareness so that we can know what we already know and be who we already are.”
+         </p>   
         </div>
-  
-    </>
+        <div className="col-sm-6">
+         <Card style={{ maxWidth: '35rem' }}>
+         <Card.Img variant="top" src="https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1520&q=80"  alt="Activity logo 1"/>
+       
+         </Card> 
+       </div>  
+      </div>
+       <br/><br/>    
+      <div className="row">
+        <div className="col-sm-6">
+         <Card style={{ maxWidth: '35rem' }}>
+         <Card.Img variant="top" src="https://images.unsplash.com/photo-1551632811-561732d1e306?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGlraW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60"  alt="Activity logo 2"/>
+         
+         </Card> 
+       </div>  
+
+       <div className="col-sm-6">
+          <p>  
+          “Some mountains only require a good pair of shoes. Others require an entire team to conquer. Knowing which is which, is the key to success.”– We Dream of Travel
+         </p>   
+        </div>
+      </div>
+<br/><br/>
+      <div className="row">
+         <div className="col-sm-6">
+          <p  >  
+          “Believe in yourself, not only in swimming but in life itself. You always have to have fun. You have to have an open mind. If you’re not enjoying it, don’t do it. Life’s too short.” – Debbie Meyer
+         </p>   
+        </div>
+        <div className="col-sm-6">
+         <Card style={{ maxWidth: '35rem' }}>
+         <Card.Img variant="top" src="https://images.unsplash.com/photo-1578253734010-32bb761af7e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1752&q=80"  alt="Activity logo 1"/>
+        
+         </Card> 
+       </div>  
+      </div>
+
+<br/><br/>
+      <div className="row">
+        <div className="col-sm-6">
+         <Card style={{ maxWidth: '35rem' }}>
+         <Card.Img variant="top" src="https://images.unsplash.com/photo-1541625602330-2277a4c46182?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"  alt="Activity logo 2"/>
+        
+         </Card> 
+       </div>  
+
+       <div className="col-sm-6 " >
+          <p >  
+          “Life is like riding a bicycle. In order to keep your balance, you must keep moving.” — Albert Einstein, genius         </p>   
+        </div>
+      </div>
+
+    </div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,9 @@
-// import SocialFollow from './SocialFollow';
+import SocialFollow from './SocialFollow';
 
 export default function footer(){
     return(
         <>
-        
-        <h1> hello footer !!!</h1>
-        {/* <SocialFollow/> */}
+        <SocialFollow/>
         </>
     )
 }
