5ddae869c9b2fd42be3e9bc0d43f54434d54597d
deep <agshikso2@gmail.com>
2022-07-26 15:19:10 +0200

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,33 +2,39 @@ import SocialFollow from './SocialFollow';
 import Geolocation from './Geolocation';
 
 
-export default function footer() {
-    return (
+
+export default function footer(){
+    return(
         <>
 
-            <div className="container-fluid text-center text-md-left" >
-                <img src="./Images/FOOTER.png" alt="banner logo" />
-                <div className="row">
-
-
-                    <div className="col-md-3 pt-2 mt-3">
-                        <ul className="list-unstyled pt-3 text-bottom .text-white">
-                            <li><a href="#!">Home</a></li>
-                            <li><a href="#!">About </a></li>
-                            <li><a href="#!">Contact </a></li>
-                            <li><a href="#!">Feedback</a></li>
-                        </ul>
-                    </div>
-                </div>
+    <div className="container-fluid text-center text-md-left " style={{ color: "white", background: "#073648" }}  >
+        <div className="row">
+            <div className="img-fluid ">
+                <img src="./images/footImage.png" alt="map image"/>
             </div>
 
-            <div className="footer-copyright text-center py-3">© 2020 Copyright:
-                <a href="https://walking-tales.com/"> walking-tales.com</a>
+            <div className="col-md-3 pt-2 mt-3">
+                <ul className="list-unstyled pt-3 text-bottom .text-white">
+                    <li><a href="#!">Home</a></li>
+                    <li><a href="#!">About </a></li>
+                    <li><a href="#!">Contact </a></li>
+                    <li><a href="#!">Feedback</a></li>
+                </ul>
             </div>
 
+           
+            <SocialFollow/>
+        </div>
+    </div>
+
+    <div className="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://walking-tales.com/"> walking-tales.com</a>
+    </div>
 
 
 
+       
         </>
     )
 }
+
