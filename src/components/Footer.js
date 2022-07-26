3e8ba13e015553542f8abfa497a562971f74eedc
deep <agshikso2@gmail.com>
2022-07-26 16:54:23 +0200

    merge conflict removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,39 @@
 import SocialFollow from './SocialFollow';
-import Geolocation from './Geolocation';
 
 
 
-export default function footer(){
-    return(
+
+export default function footer() {
+    return (
         <>
 
-    <div className="container-fluid text-center text-md-left " style={{ color: "white", background: "#073648" }}  >
-        <div className="row">
-            <div className="img-fluid ">
-                <img src="./images/footImage.png" alt="map image"/>
+            <div className="container-fluid text-center text-md-left " style={{ color: "white", background: "#073648" }}  >
+                <div className="row">
+                    <div className="img-fluid ">
+                        <img src="./images/footImage.png" alt="map image" />
+                    </div>
+
+                    <div className="col-md-3 pt-2 mt-3">
+                        <ul className="list-unstyled pt-3 text-bottom .text-white">
+                            <li><a href="#!">Home</a></li>
+                            <li><a href="#!">About </a></li>
+                            <li><a href="#!">Contact </a></li>
+                            <li><a href="#!">Feedback</a></li>
+                        </ul>
+                    </div>
+
+
+                    <SocialFollow />
+                </div>
             </div>
 
-            <div className="col-md-3 pt-2 mt-3">
-                <ul className="list-unstyled pt-3 text-bottom .text-white">
-                    <li><a href="#!">Home</a></li>
-                    <li><a href="#!">About </a></li>
-                    <li><a href="#!">Contact </a></li>
-                    <li><a href="#!">Feedback</a></li>
-                </ul>
+            <div className="footer-copyright text-center py-3">© 2020 Copyright:
+                <a href="https://walking-tales.com/"> walking-tales.com</a>
             </div>
 
-           
-            <SocialFollow/>
-        </div>
-    </div>
-
-    <div className="footer-copyright text-center py-3">© 2020 Copyright:
-        <a href="https://walking-tales.com/"> walking-tales.com</a>
-    </div>
 
 
 
-       
         </>
     )
 }
