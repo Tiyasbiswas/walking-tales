ec12744c241a2cc8524303bc5b3b49dec9ca4d45
Tiyasbiswas <tiyasbiswas17@gmail.com>
2022-07-26 09:29:03 +0200

    Geolocation Added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,11 @@
 import SocialFollow from './SocialFollow';
+import Geolocation from './Geolocation';
+
 
 export default function footer(){
     return(
         <>
+        <Geolocation/>
         <SocialFollow/>
         </>
     )
---
+++ src/components/Geolocation.js
@@ -0,0 +1,30 @@
+import { MapContainer, TileLayer, Marker,Popup } from 'react-leaflet'
+import React ,{ useState,useEffect}from 'react';
+
+ export default function Geolocation () {
+    const [latitude,setLatitude]=useState(0);
+    const [longitude,setLongitude]=useState(0);
+    
+    useEffect(() =>{
+        navigator.geolocation.getCurrentPosition((position)=>{
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        })
+    },[])
+    return (
+        <>
+        <div className='container-map'>
+            <MapContainer center={[latitude, longitude]} zoom={2} scrollWheelZoom={false}>
+                <TileLayer
+                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"           
+                />
+                <Marker position={[latitude, longitude]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+            </div>
+        </>
+    )
+}
