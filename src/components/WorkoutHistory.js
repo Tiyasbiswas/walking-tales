61a486252c87aa60079a160fbfe998f8aa10069b
Tiyasbiswas <tiyasbiswas17@gmail.com>
2022-08-05 12:38:53 +0200

    Workout history pie chart added

--- src/components/WorkoutHistory.js
+++ src/components/WorkoutHistory.js
@@ -4,82 +4,115 @@ import { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import TableauReport from "tableau-react";
+import { VictoryPie } from "victory-pie";
+import { VictoryChart } from "victory-chart";
+import { VictoryTooltip } from "victory-tooltip";
 
 
-const options = {
-  height: 800,
-  width: 800,
-  hideTabs: false
-  // All other vizCreate options are supported here, too
-  // They are listed here: https://onlinehelp.tableau.com/current/api/js_api/en-us/JavaScriptAPI/js_api_ref.htm#ref_head_9
-};
-
-const filters = {
-  Colors: ["Blue", "Red"],
-  Sizes: ["Small", "Medium"]
-};
-
-const parameters = {
-  Param1: "Value",
-  Param2: "Other Value"
-};
 
  function WorkoutHistory(props){
   const [value, onChange] = useState(new Date());
 
-  const today = new Date(),
-   date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+   const [data3, setData3] = useState([
+    { x: "walking", y: 330 },
+    { x: "Running", y: 180 },
+    { x: "Swimming", y: 200 },
+    { x: "Hiking", y: 120 },
+    { x: "Dancing", y: 100 },
+    { x: "cycling", y: 220 }
+
+  ]);
+
+  const [label, setLabel] = useState(false);
+
+  const handleClick = () => {
+    const updated = [...data3];
+    updated[0].y += 700;
+
+    console.log(updated);
+
+    setData3(updated);
+  };
+
+  const handleToggler = () => {
+    setLabel((val) => !val);
+  };
 
 
 return(
 <>
 <div><Header/></div>
 
-<div className='tabhistory'>
-<div>
+     <div className='container'>
+      <div>
+        <p> Workout chart</p>
       <Calendar
         onChange={onChange}
         value={value}
-      /></div>
-<Tabs forceRenderTabPanel defaultIndex={1}>
-    {/* <TabList>
-      
-      <Tab>Daily</Tab>
-      <Tab>Weekly</Tab>
-    </TabList> */}
-    <TabPanel>
-      <Tabs forceRenderTabPanel>
-        {/* <TabList>
-          <Tab>CurrentDay</Tab>
-        </TabList> */}
-        <TabPanel>
-          {/* <p>{today}</p> */}
-          <p><img src="https://i.etsystatic.com/24785820/r/il/6fd684/3826900364/il_570xN.3826900364_jo4n.jpg" alt="Homer Simpson" /></p>
-          <p> 57 mins</p>
-        </TabPanel>
-      </Tabs>
-    </TabPanel>
-    <TabPanel>
-      <Tabs forceRenderTabPanel>
-        <TabList>
-          <Tab>Weekly</Tab>
-        </TabList>
-        <TabPanel>
-        <TableauReport
-      url="https://public.tableau.com/views/SectorVisualizationsGender/AssetOwnership?:language=en-GB&:display_count=n&:origin=viz_share_link"
-      filters={filters}
-      parameters={parameters} 
-      options={options}
-    />
-        </TabPanel>
-      </Tabs>
-    </TabPanel>
-  </Tabs>
-</div>
+      />
+      </div>
+      <VictoryPie
+        padAngle={({ datum }) => 2}
+        animate={{
+          duration: 2000
+        }}
+        innerRadius={(val) => {
+          console.log(val.datum);
+          return val.datum.x === "x" ? 170 : 130;
+        }}
+        width="1200"
+        tooltip={"dshv"}
+        labelComponent={
+          label ? <VictoryTooltip dy={0} centerOffset={{ x: 25 }} /> : undefined
+        }
+        colorScale={["tomato", "orange", "blue", "olive", "Green","brown"]}
+        data={data3}
+      />
+      <button onClick={handleClick}>View toggle</button>
+      <button onClick={handleToggler}>Excersize Type</button>
+      </div>
 <div><Footer/></div>
 </>
  )
 }
 
 export default WorkoutHistory;
+
+
+
+// <div className='tabhistory'>
+// <Tabs forceRenderTabPanel defaultIndex={1}>
+//     {/* <TabList>
+      
+//       <Tab>Daily</Tab>
+//       <Tab>Weekly</Tab>
+//     </TabList> */}
+//     <TabPanel>
+//       <Tabs forceRenderTabPanel>
+//         {/* <TabList>
+//           <Tab>CurrentDay</Tab>
+//         </TabList> */}
+//         <TabPanel>
+//           {/* <p>{today}</p> */}
+//           <p><img src="https://i.etsystatic.com/24785820/r/il/6fd684/3826900364/il_570xN.3826900364_jo4n.jpg" alt="Homer Simpson" /></p>
+//           <p> 57 mins</p>
+//         </TabPanel>
+//       </Tabs>
+//     </TabPanel>
+//     <TabPanel>
+//       <Tabs forceRenderTabPanel>
+//         <TabList>
+//           <Tab>Weekly</Tab>
+//         </TabList>
+//         <TabPanel>
+//         <TableauReport
+//       url="https://public.tableau.com/views/SectorVisualizationsGender/AssetOwnership?:language=en-GB&:display_count=n&:origin=viz_share_link"
+//       filters={filters}
+//       parameters={parameters} 
+//       options={options}
+//     />
+//         </TabPanel>
+//       </Tabs>
+//     </TabPanel>
+//   </Tabs>
+// </div>
